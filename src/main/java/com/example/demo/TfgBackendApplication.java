22fb3399e57187a3646564eb4ac4fbf6b3772eac
santogarrido <santocildesgarridoraul@gmail.com>
2026-04-17 11:02:39 +0200

    Project creation

---
+++ src/main/java/com/example/demo/TfgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgBackendApplication.class, args);
+	}
+
+}
